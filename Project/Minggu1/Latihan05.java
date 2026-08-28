23b5dfff3487771b3fc294e3a797a46a06a823ea
athasyah <athasyahaddin24@gmail.com>
2026-08-28 15:19:48 +0700

    up java week1

---
+++ Project/Minggu1/Latihan05.java
@@ -0,0 +1,9 @@
+public class Latihan05 {
+
+    public static void main(String[] args) {
+        System.out.println("Halo, nama saya adalah Athasyah Addin Satriya Abdi");
+
+        System.out.print("\nBelajar java itu mudah!");
+        System.out.print("\nSemakin sering mencoba, semakin jago");
+    }
+}
